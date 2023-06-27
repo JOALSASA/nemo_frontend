0fbfeefa502f6859ce0b230c0a635c94cc609ac6
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2023-06-26 23:35:02 -0300

    Adicionado a opção de sair

--- lib/components/appbars/primary_app_bar.dart
+++ lib/components/appbars/primary_app_bar.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:nemo_frontend/components/buttons/app_bar_text_button.dart';
 import 'package:nemo_frontend/components/utils/PaletaCores.dart';
+import 'package:nemo_frontend/main.dart';
+import 'package:nemo_frontend/utils/local_storage.dart';
 import 'package:nemo_frontend/view/internal/alertas/alertas_view.dart';
-import 'package:nemo_frontend/view/internal/conta/conta_view.dart';
 import 'package:nemo_frontend/view/internal/home_view.dart';
 
 class PrimaryAppBar extends AppBar {
@@ -62,13 +63,21 @@ class PrimaryAppBar extends AppBar {
                   if (telaConta) {
                     return;
                   }
-
-                  Navigator.of(context).pushReplacement(PageRouteBuilder(
-                    pageBuilder: (context, animation1, animation2) =>
-                        const ContaView(),
-                    transitionDuration: Duration.zero,
-                    reverseTransitionDuration: Duration.zero,
-                  ));
+                  showMenu(
+                    context: context,
+                    position: const RelativeRect.fromLTRB(100, 80, 10, 100),
+                    items: [
+                      PopupMenuItem<String>(
+                        child: const Text('Sair'),
+                        onTap: () async {
+                          await LocalStorage.clearStorage();
+                          navigatorKey.currentState!
+                              .pushReplacementNamed('/boas-vindas');
+                        },
+                      ),
+                    ],
+                    elevation: 8.0,
+                  );
                 }),
           ],
         );
